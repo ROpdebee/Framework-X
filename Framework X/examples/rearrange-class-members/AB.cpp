6d06a890702776edd7753d86e3a462b4b9cae48b
ROpdebee <ropdebee@vub.ac.be>
2017-06-28 00:57:58 +0200

    Fix/extend some examples

--- Framework X/examples/rearrange-class-members/AB.cpp
+++ Framework X/examples/rearrange-class-members/AB.cpp
@@ -10,15 +10,16 @@ public:
 class B {
     const int a;
     bool _foo;
+    int fooInt() { return static_cast<int>(_foo); }
 public:
     B(int _a, bool foo) : a(_a), _foo(foo) {}
-    B(const B &otherB) : a(otherB.a), _foo(otherB._foo) {}
+    B(const B &b) : a(b.a), _foo(b._foo) {}
     
     bool foo() { return _foo; }
     void foo(bool newFoo) { _foo = newFoo; }
     int get_a() { return a; }
     
     int performComputation() {
-        return a + static_cast<int>(_foo);
+        return a + fooInt();
     }
 };
